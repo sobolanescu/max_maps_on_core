8a55ee9ce5b710884ba50c5b0362bd954ac4a00a
sobolanescu <38504992+sobolanescu@users.noreply.github.com>
2021-01-13 01:01:23 +0200

    Create tables.h

---
+++ server/common/tables.h
@@ -0,0 +1,23 @@
+In:
+
+typedef struct SPacketGDSetup
+
+Search:
+
+	long	alMaps[32];
+
+Change in:
+
+	long	alMaps[MAX_MAP_ALLOW];
+
+In:
+
+typedef struct SMapLocation
+
+Search:
+
+	long	alMaps[32];
+
+Change in:
+
+	long	alMaps[MAX_MAP_ALLOW];
